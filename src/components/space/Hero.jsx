a10789d1c103800e4c77f483ad4104a29aff9c6c
Davonne Vigil <davonnejv@gmail.com>
2025-01-06 17:06:53 -0600

    feature: search bar with styles

--- src/components/space/Hero.jsx
+++ src/components/space/Hero.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import Search from "./Search";
 
 export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center text-white mt-40 text-xl md:text-4xl lg:text-5xl font-serif font-bold">
-      <div className="max-w-xs md:max-w-4xl lg:max-w-4xl bg-slate-800 opacity-75 p-2 md:p-8 rounded-xl">
+      <div className="max-w-xs md:max-w-4xl lg:max-w-4xl bg-slate-800 opacity-75 p-2 md:p-10 rounded-xl">
         <h2>
           Help us Save Humanity <br />
           <div className="mt-2 md:mt-4">
@@ -14,12 +14,7 @@ export default function Hero() {
             </span>
           </div>
         </h2>
-
-        <p className="font-thin mt-4 lg:mt-8 text-md lg:text-2xl">
-          Combing our forces to bring together a group of individuals who care
-          about humanity's survival of the apocalypse. Bring your family and
-          friends and lets surive the end together.
-        </p>
+        <Search />
       </div>
     </section>
   );
---
+++ src/components/space/Search.jsx
@@ -0,0 +1,27 @@
+import Button from "./Button";
+
+export default function Search() {
+  return (
+    <section>
+      <p className="font-thin mt-4 lg:mt-8 text-md lg:text-2xl">
+        Combing our forces to bring together a group of individuals who care
+        about humanity's survival of the apocalypse. Bring your family and
+        friends and lets surive the end together.
+      </p>
+      <label>
+        <input
+          type="search"
+          //   value={inputtedSearch}  //need state to practice
+          placeholder="Search Locations"
+          className="bg-white text-slate-800 max-w-7xl rounded p-3 mt-4 text-sm lg:text-lg  shadow-md focus:outline-none focus:ring focus:ring-orange-500"
+        />
+        <Button
+          btnTxt={"Explore"}
+          className={
+            "bg-white cursor-pointer text-slate-800 p-4 mt-4 md:mt-2 text-sm ml-2"
+          }
+        />
+      </label>
+    </section>
+  );
+}
