1589f98dd543225c3c6055f1ee9d03a8bb92702f
Davonne Vigil <davonnejv@gmail.com>
2025-01-11 21:38:44 -0600

    add more styles to nav bar, add black background, add icon for mobile nav

--- src/components/wanna/Nav.jsx
+++ src/components/wanna/Nav.jsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
+
 export default function Nav() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   const myNav = [
-    { link: "Consistent", id: 1 },
+    { link: "Consistency", id: 1 },
     { link: "Practice", id: 2 },
     { link: "Participation", id: 3 },
     { link: "Showcase", id: 4 },
   ];
   return (
     <>
-      <header className="flex flex-row font-bold text-purple-600 gap-8">
-        <h1 className="text-3xl">Wannaa Coode?</h1>
+      <header className="flex flex-row justify-between items-center w-full p-4 font-arc text-purple-600 gap-8 mt-4">
+        <h1 className="text-4xl">Wanna Coode?</h1>
         <nav>
-          <ul className="flex gap-8 ">
+          <ul className="hidden lg:flex gap-8 text-2xl">
             {myNav.map((nav) => (
               <li key={nav.id}>{nav.link}</li>
             ))}
           </ul>
+
+          <button className="text-3xl lg:hidden cursor-pointer">
+            {isNavOpen ? "x" : "☰"}
+          </button>
         </nav>
       </header>
     </>
--- src/components/wanna/WannaCode.jsx
+++ src/components/wanna/WannaCode.jsx
@@ -2,8 +2,10 @@ import Nav from "./Nav";
 
 export default function WannaCode() {
   return (
-    <main className="flex flex-col justify-center items-center">
-      <Nav />
+    <main className=" bg-black min-h-screen">
+      <section className="flex flex-col justify-center items-center ml-auto">
+        <Nav />
+      </section>
     </main>
   );
 }
