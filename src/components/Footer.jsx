fc0d52d38f7035f2360688f276a8e54b30d23ebb
Davonne Vigil <davonnejv@gmail.com>
2025-01-11 19:25:57 -0600

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mt-auto bg-white w-full text-center text-xl font-glo p-2 text-purple-600">
-      Djvd Web Services Inc.
+      © coded and designed by davonne vigil
     </footer>
   );
 }
