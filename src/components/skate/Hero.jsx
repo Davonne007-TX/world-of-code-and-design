8da9e69ac9810bdcd21dce489da72915e7954d5c
Davonne Vigil <davonnejv@gmail.com>
2025-01-14 17:32:19 -0600

    add hero

---
+++ src/components/skate/Hero.jsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+  const aboutSkateCompetition = [
+    `2025 brings our Annual National Skating Skate Off. Skaters from across
+            the United States come together to compete for glory and the chance to
+            win $50,000 worth of skating gear, $20,000, and a chance for National Sponsorship.`,
+  ];
+
+  return (
+    <section className="flex flex-col justify-center items-center text-white bg-black/50 p-8 rounded lg:mt-40">
+      <section className="max-w-4xl">
+        <h2 className="text-7xl mt-20 font-pro">Do you have what it takes?</h2>
+        <p className="text-2xl mt-4 font-sans">{aboutSkateCompetition}</p>
+      </section>
+    </section>
+  );
+}
--- src/components/skate/SkateCompetition.jsx
+++ src/components/skate/SkateCompetition.jsx
@@ -1,9 +1,11 @@
 import SkateHeader from "./SkateHeader";
+import Hero from "./Hero";
 
 export default function SkateCompetition() {
   return (
     <main className="bg-[url('/images/skate.jpg')] bg-cover min-h-screen">
       <SkateHeader />
+      <Hero />
     </main>
   );
 }
