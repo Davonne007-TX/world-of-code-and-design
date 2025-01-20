539a436ed6686bab346be66378dbd32adee8b243
Davonne Vigil <davonnejv@gmail.com>
2025-01-20 14:19:48 -0600

    add header for fiftes project

--- src/components/grease/FiftesHeader.jsx
+++ src/components/grease/FiftesHeader.jsx
@@ -1,7 +1,21 @@
 export default function FiftesHeader() {
+  const fiftesNav = [
+    { ourLink: "Shop", id: 1 },
+    { ourLink: "Gallery", id: 2 },
+    { ourLink: "Classics", id: 3 },
+    { ourLink: "Dealers", id: 4 },
+  ];
+
   return (
-    <header>
-      <h1>Systematic</h1>
-    </header>
+    <nav className="flex gap-8 justify-center items-center font-orb text-white">
+      <ul className="text-xl flex gap-10 mt-4 font-bold  bg-pink-800 p-4 rounded-full opacity-75">
+        <h1 className="md:text-3xl">Systematic</h1>
+        {fiftesNav.map((nav) => (
+          <li key={nav.id} className="lg:mt-2">
+            {nav.ourLink}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
