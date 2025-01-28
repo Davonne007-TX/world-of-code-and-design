83972eb8c1a6117f56b9a8120f5dfb98fdad3a41
Davonne Vigil <davonnejv@gmail.com>
2025-01-27 22:51:54 -0600

    feature header with styles

--- src/components/spiderman/SpidermanHeader.jsx
+++ src/components/spiderman/SpidermanHeader.jsx
@@ -1,9 +1,43 @@
+import Image from "../Image";
+
 export default function SpidermanHeader() {
+  const fanClubLinks = [
+    { spidermanLink: "About", id: 1 },
+    { spidermanLink: "Chat", id: 2 },
+    { spidermanLink: "Gallery", id: 3 },
+    { spidermanLink: "Merch", id: 4 },
+  ];
+
+  const cart = "./images/redCart.png";
+  const login = "./images/redAccount.png";
+
   return (
-    <header className="text-white flex justify-center items-center">
-      <div className="mt-8">
-        <h1 className="font-press text-3xl md:text-5xl">Spiderman Fan Club</h1>
-      </div>
+    <header className="text-white flex flex-col justify-center items-center">
+      <section className="flex justify-between items-center w-full mt-2 p-4">
+        <h1 className="font-press text-2xl md:text-3xl ">Spiderman Fan Club</h1>
+
+        <nav className="hidden md:flex justify-center">
+          <ul className="flex ">
+            {fanClubLinks.map((link) => (
+              <li
+                key={link.id}
+                className="hover:underline text-xl font-sans mr-8"
+              >
+                {link.spidermanLink}
+              </li>
+            ))}
+          </ul>
+
+          <section className="hidden md:flex ml-auto text-lg">
+            <Image
+              image={login}
+              className={" w-8"}
+              alt={"red account, icon by icons8 "}
+            />
+            <Image image={cart} alt={"red cart icon, icon by icons8"} />
+          </section>
+        </nav>
+      </section>
     </header>
   );
 }
