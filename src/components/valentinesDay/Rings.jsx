6af11ee2b30f9708dcbe2bd63dc1d02f4ce28964
Davonne Vigil <davonnejv@gmail.com>
2025-02-06 22:49:37 -0600

    update mobie responsiveness

--- src/components/valentinesDay/Rings.jsx
+++ src/components/valentinesDay/Rings.jsx
@@ -10,9 +10,9 @@ export default function Rings() {
   return (
     <section className="flex justify-center items-center">
       <div className="relative max-w-6xl">
-        <Image image="./images/rings.jpg" className="w-full rounded-3xl" />
+        <Image image="./images/rings.jpg" className="w-full rounded-3xl mt-4" />
 
-        <section className="absolute top-0 left-0 w-full font-thin mt-10 md:mt-40 flex flex-col justify-center items-center text-black px-4">
+        <section className="absolute lg:top-0 left-0 w-full font-thin mt-10 md:mt-40 flex flex-col justify-center items-center text-black px-4">
           <p className="text-4xl font-thin text-center">
             Get Your Last Minute Valentines Day Gift
           </p>
