c15e3342e78f089a3e1c9696b62b386561b2dd75
Davonne Vigil <davonnejv@gmail.com>
2025-02-11 19:36:28 -0600

    update styles for mobile responsiveness

--- src/components/astro/AstroHeader.jsx
+++ src/components/astro/AstroHeader.jsx
@@ -1,6 +1,6 @@
 export default function AstroHeader() {
   const spaceNav = [
-    { spaceLink: "Astronaut Training Info Session", id: 1 },
+    { spaceLink: "Astronaut Training", id: 1 },
     { spaceLink: "Simulations", id: 2 },
     { spaceLink: "Space Missions", id: 3 },
     { spaceLink: "FAQ", id: 4 },
@@ -15,10 +15,12 @@ export default function AstroHeader() {
   ];
 
   return (
-    <header className="text-white bg-black/50 w-full py-4">
+    <header className="text-white bg-black/60 w-full py-4">
       <section className="flex justify-between items-center px-4">
-        <h1 className="text-4xl font-audio">Next Generation Astronaut</h1>
-        <ul className="ml-auto mt-4 flex gap-4 text-md">
+        <h1 className="text-3xl md:text-4xl font-audio">
+          Next Generation Astronaut
+        </h1>
+        <ul className="ml-auto mt-4 hidden md:flex gap-4 text-xl cursor-pointer">
           {resources.map((source) => (
             <li key={source.id} className="hover:text-gray-300 transition">
               {source.resource}
@@ -28,7 +30,7 @@ export default function AstroHeader() {
       </section>
 
       <nav>
-        <ul className="flex ml-4 gap-8 text-xl font-thin py-2">
+        <ul className="flex flex-col md:flex-row ml-4 mt-2 gap-8 text-xl font-thin py-2  cursor-pointer">
           {spaceNav.map((nav) => (
             <li key={nav.id} className="hover:text-gray-300 transition">
               {nav.spaceLink}
