b516acf9f7f28b08f15ebffb3e096796ffdd5271
Davonne Vigil <davonnejv@gmail.com>
2025-02-11 18:12:13 -0600

    add hero section

--- src/components/astro/AstroHeader.jsx
+++ src/components/astro/AstroHeader.jsx
@@ -1,14 +1,14 @@
 export default function AstroHeader() {
   const spaceNav = [
     { spaceLink: "Astronaut Training Session", id: 1 },
-    { spaceLink: "Similations", id: 2 },
+    { spaceLink: "Simulations", id: 2 },
     { spaceLink: "Space Missions", id: 3 },
-    { spaceLink: "Apply Now", id: 4 },
+    { spaceLink: "Q&A", id: 4 },
   ];
 
   return (
     <header className="p-4 text-white">
-      <h1 className="text-7xl font-audio">Next Generation Astronaut</h1>
+      <h1 className="text-5xl font-audio">Next Generation Astronaut</h1>
 
       <nav>
         <ul className="flex gap-12 text-2xl p-2 mt-4 font-thin">
--- src/components/astro/NextGenerationAstro.jsx
+++ src/components/astro/NextGenerationAstro.jsx
@@ -1,9 +1,19 @@
 import AstroHeader from "./AstroHeader";
+import Button from "../Button";
 
 export default function NextGenerationAstro() {
   return (
-    <main className="bg-[url('./images/nextAstro.jpg')] bg-cover min-h-screen">
+    <main className="bg-[url('./images/nextAstro.jpg')] font-thin flex flex-col text-white bg-cover min-h-screen">
       <AstroHeader />
+      <section className="flex flex-col bg-black/10 justify-center items-center">
+        <h2 className="font-thin mt-28 text-4xl">
+          Learn from our experts. Train with the best. Be the best.
+        </h2>
+        <Button
+          btnTxt={"Apply Today"}
+          className={"mt-4 bg-teal-900 p-2 rounded-full w-40 font-audio"}
+        />
+      </section>
     </main>
   );
 }
