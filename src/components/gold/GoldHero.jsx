5953e3de6d4e1ed70f0e895a2911f63bd7553cfd
Davonne Vigil <davonnejv@gmail.com>
2025-03-11 01:01:23 -0500

    update code for mobile resonsiveness

--- src/components/gold/GoldHero.jsx
+++ src/components/gold/GoldHero.jsx
@@ -1,12 +1,15 @@
 import Button from "../Button";
 
+const help = "Let Us, Help You";
+const excuses = "1-800-NO-EXCUSESs";
+
 export default function GoldHero() {
   return (
-    <section className="flex justify-center items-center text-center mt-60">
-      <section className="max-w-3xl bg-green-600  text-white p-2 rounded-xl">
+    <section className="flex justify-center items-center text-center mt-80 md:mt-60">
+      <section className="max-w-3xl bg-green-600  text-white p-2 rounded-xl mb-8">
         <h2 className="font-audio text-4xl">Chase After Your Goal ..!</h2>
-        <p className="font-audio text-3xl mt-4">Let Us, Help You</p>
-        <p className="font-bold text-3xl mt-4">1-800-NO-EXCUSES</p>
+        <p className="font-audio text-3xl mt-4">{help}</p>
+        <p className="font-bold text-3xl mt-4">{excuses}</p>
         <Button
           btnTxt={"Pledge Today"}
           className={
