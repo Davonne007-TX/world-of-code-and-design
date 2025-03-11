5682ebdf7e977814a4f7a6f111028745f0a08140
Davonne Vigil <davonnejv@gmail.com>
2025-03-11 01:10:20 -0500

    test complete

--- src/components/gold/GoldHero.jsx
+++ src/components/gold/GoldHero.jsx
@@ -7,7 +7,7 @@ export default function GoldHero() {
   return (
     <section className="flex justify-center items-center text-center mt-80 md:mt-60">
       <section className="max-w-3xl bg-green-600  text-white p-2 rounded-xl mb-8">
-        <h2 className="font-audio text-4xl">Go After Your Goal ..!</h2>
+        <h2 className="font-audio text-4xl">Chase After Your Goal ..!</h2>
         <p className="font-audio text-3xl mt-4">{help}</p>
         <p className="font-bold text-3xl mt-4">{excuses}</p>
         <Button
