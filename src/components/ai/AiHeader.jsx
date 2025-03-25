0a0afad8ced9581d9a49dc086fe7ad28d8771bd0
Davonne Vigil <davonnejv@gmail.com>
2025-03-25 01:43:25 -0500

    add AIButtons Component with styles

--- src/components/ai/AiHeader.jsx
+++ src/components/ai/AiHeader.jsx
@@ -8,12 +8,12 @@ export default function AiHeader() {
     { link: "Purchasing", id: 4 },
   ];
   return (
-    <header className="p-4 flex flex-col gap-2">
-      <div className="flex gap-60">
+    <header className="p-4 flex flex-col  gap-2">
+      <div className="flex gap-60 ">
         <h1 className={"text-4xl font-orb"}>R. Force</h1>
-        <AIButtons />
+        {/* <AIButtons /> */}
       </div>
-      <ul className="flex gap-8 text-xl font-thin">
+      <ul className="flex gap-8 text-2xl font-thin">
         {aiFeatures.map((features) => (
           <li key={features.id}>{features.link}</li>
         ))}
