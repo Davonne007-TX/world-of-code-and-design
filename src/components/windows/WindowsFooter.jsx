3126dabea19560dd61b2d7b4986af7b69342aa42
Davonne Vigil <davonnejv@gmail.com>
2025-04-03 23:30:29 -0500

    -

--- src/components/windows/WindowsFooter.jsx
+++ src/components/windows/WindowsFooter.jsx
@@ -5,7 +5,7 @@ export default function WindowsFooter() {
     <footer className="bg-brightPink p-6 mt-4 flex justify-between items-center">
       <Image image="/images/power.png" className="w-12" />
       <p className="font-lobster ml-4 text-2xl">Turn off Computer</p>
-      <p className="ml-auto mr-4 max-w-lg text-xl font-sans">
+      <p className="ml-auto mr-4  max-w-lg text-xl font-sans">
         You are able to switch accounts. You can also add more users or change
         any. Head on over to User Settings.
       </p>
