191587567ddc004875e5cf2967d0d561d1f6be78
Davonne Vigil <davonnejv@gmail.com>
2025-04-07 22:35:56 -0500

    feature: hero with styles

--- src/components/eat/EatHero.jsx
+++ src/components/eat/EatHero.jsx
@@ -1,16 +1,22 @@
+import Button from "../Button";
 import Card from "../Card";
 
 export default function EatHero() {
   return (
-    <section className="max-w-4xl mx-auto">
-      <Card
-        title="Ultra Fast Delievery Service"
-        className="text-white font-sig mx-auto"
-        description="Get Your Food delieved on your terms.
-          When you want, how you want, for ulimate deliciosness.
-          "
-        descriptionStyles="text-white font-serif text-center"
-      />
+    <section className="flex flex-col justify-center items-center text-center mt-10">
+      <div className="max-w-4xl w-full flex flex-col items-center space-y-4">
+        <Card
+          title="Ultra Fast Delievery Service"
+          className="text-white font-sig"
+          description="Get Your Food delieved on your terms.
+          When you want, how you want, for ulimate deliciosness."
+          descriptionStyles="text-white font-serif text-center"
+        />
+        <Button
+          btnTxt="Order Classic Favorites"
+          className="bg-white text-black rounded-full p-2 font-bold w-80"
+        />
+      </div>
     </section>
   );
 }
