dc1c51b21857708f8f2f42ab9d9d08ab84444264
Davonne Vigil <davonnejv@gmail.com>
2025-04-07 20:49:57 -0500

    add alternative text to images on Windows UI Project

--- src/components/windows/Users.jsx
+++ src/components/windows/Users.jsx
@@ -2,10 +2,30 @@ import ChooserAUser from "./ChooserAUser";
 
 export default function Users() {
   const users = [
-    { userName: "Cat", userImage: "/images/cat.jpg", alt: "", id: 1 },
-    { userName: "Code", userImage: "/images/coding.jpg", alt: "", id: 2 },
-    { userName: "Duck", userImage: "/images/duck.jpg", alt: "", id: 3 },
-    { userName: "Plane", userImage: "/images/plane.jpg", alt: "", id: 4 },
+    {
+      userName: "Cat",
+      userImage: "/images/cat.jpg",
+      alt: "Cat laying upside down, Photo by: Pixbay from Pexels",
+      id: 1,
+    },
+    {
+      userName: "Code",
+      userImage: "/images/coding.jpg",
+      alt: "Laptop with code, Photo by Oluwaseun Duncan on Pexels",
+      id: 2,
+    },
+    {
+      userName: "Duck",
+      userImage: "/images/duck.jpg",
+      alt: "Yellow Rubber Duck, Photo by Neosiam 2024 on Pexels",
+      id: 3,
+    },
+    {
+      userName: "Plane",
+      userImage: "/images/plane.jpg",
+      alt: "Plane Getting Ready to Board, Photo by Marina Hinic on Pexels",
+      id: 4,
+    },
   ];
 
   return (
