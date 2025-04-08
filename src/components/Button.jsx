0e33905be373c2a08226d0b230250f60f6de9864
Davonne Vigil <davonnejv@gmail.com>
2025-04-08 11:55:47 -0500

    add mobile nav bar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,10 @@
-export default function Button({ className, type, btnTxt }) {
+export default function Button({ className, type, btnTxt, onClick }) {
   return (
-    <button type={type} className={`${className} cursor-pointer`}>
+    <button
+      type={type}
+      className={`${className} cursor-pointer`}
+      onClick={onClick}
+    >
       {btnTxt}
     </button>
   );
--- src/components/eat/EatNowHeader.jsx
+++ src/components/eat/EatNowHeader.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 export default function EatNowHeader() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  const closeMobileNav = () => {
+    setIsMobileNavOpen(false);
+  };
+
+  const foodLinks = [
+    { ourLink: "Top Restaurants", id: 1 },
+    { ourLink: "Fast Food", id: 2 },
+    { ourLink: "Catering", id: 3 },
+    { ourLink: "Dessert", id: 4 },
+    { ourLink: "Coffee", id: 5 },
+  ];
   return (
-    <header className="text-white p-8">
+    <header className="text-white p-8 flex justify-between ">
       <h1 className="font-sig text-5xl">Eat Now</h1>
+      <button onClick={toggleMobileNav} className="text-5xl cursor-pointer">
+        {" "}
+        ☰
+      </button>
+
+      <nav
+        className={`${
+          isMobileNavOpen
+            ? "flex flex-col text-3xl text-white font-sig absolute right-0 top-24 bg-red-600 p-4 rounded-2xl"
+            : "hidden"
+        }`}
+      >
+        {foodLinks.map((links) => (
+          <li key={links.id} className="list-none p-2" onClick={closeMobileNav}>
+            {links.ourLink}
+          </li>
+        ))}
+      </nav>
     </header>
   );
 }
