fc8468440c41f0545856307ae70e06bea85173b9
Davonne Vigil <davonnejv@gmail.com>
2025-04-24 16:54:44 -0500

    update header in Funky Taco Project

--- src/components/funkyTaco/TacoHeader.jsx
+++ src/components/funkyTaco/TacoHeader.jsx
@@ -4,29 +4,37 @@ export default function TacoHeader() {
   const ourLinks = [
     { tacoLinks: "Tacos", id: 1 },
     { tacoLinks: "Jumbo Tacos", id: 2 },
-    { tacoLinks: "Speciality Tacos", id: 3 },
-    { tacoLinks: "Surpreme Tacos", id: 4 },
+    { tacoLinks: "Specialty Tacos", id: 3 },
+    { tacoLinks: "Supreme Tacos", id: 4 },
   ];
 
   return (
-    <header className="p-4 text-white flex justify-center items-center">
-      <div className="flex flex-col justify-between  w-full gap-4">
-        <div className="flex justify-between items-center w-full">
-          <h1 className="font-press text-3xl ">Did Someone Say Tacos?</h1>
-          <Button
-            btnTxt="Order Now"
-            className="bg-red-500 p-2 justtify-end rounded-full w-40  font-press text-md"
-          />
-        </div>
+    <header className="text-white p-8 flex justify-between items-center bg-red-500/80">
+      <div className="flex items-center gap-8">
+        <h1 className="font-press text-5xl">🌮</h1>
         <nav>
           <ul className="flex gap-8">
             {ourLinks.map((link) => (
-              <li key={link.id} className=" hidden md:flex text-2xl">
+              <li
+                key={link.id}
+                className="hidden cursor-pointer hover:bg-red-700 p-2 transition-all duration-300  md:flex text-2xl"
+              >
                 {link.tacoLinks}
               </li>
             ))}
           </ul>
         </nav>
+      </div>
+
+      <div className="flex gap-8">
+        <Button
+          btnTxt="Locations"
+          className="bg-red-500 hover:bg-red-700 cursor-pointer p-2 rounded-full w-40 font-press text-md"
+        />
+        <Button
+          btnTxt="Order Now"
+          className="bg-red-500 p-2 hover:bg-red-700  cursor-pointer rounded-full w-40 font-press text-md"
+        />
       </div>
     </header>
   );
