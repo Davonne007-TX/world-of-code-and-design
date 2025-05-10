18d49ea8b470eef6a873b3e3e4d516eea51aa0ac
Davonne Vigil <davonnejv@gmail.com>
2025-05-10 00:00:52 -0500

    feature hero with styles and mobile responsiveness

--- src/components/concert/ConcertHeader.jsx
+++ src/components/concert/ConcertHeader.jsx
@@ -9,12 +9,12 @@ export default function ConcertHeader() {
   ];
 
   return (
-    <header className=" text-white p-6 shadow-md bg-black/10">
+    <header className=" text-white px-6 py-8 shadow-md bg-black/10">
       <div className="flex justify-between items-center">
-        <h1 className="font-sed text-3xl md:text-4xl text-shadow-md transform hover:scale-105 transition-transform duration-300">
+        <h1 className="font-sed text-4xl md:text-4xl text-shadow-md transform hover:scale-105 transition-transform duration-300">
           🎧 Takin it Back Tour
         </h1>
-        <ul className="hidden md:flex gap-10 font-sed text-2xl">
+        <ul className="hidden md:flex gap-12 font-cut text-2xl">
           {ourHeader.map((headerLink) => (
             <li
               key={headerLink.id}
@@ -26,7 +26,7 @@ export default function ConcertHeader() {
         </ul>
         <Button
           btnTxt="Get Tickets"
-          className="bg-white text-black rounded-full p-2 font-sed text-2xl transition-all duration-300 transform hover:scale-110"
+          className="hidden md:flex bg-white text-black font-sed text-2xl p-2 rounded-full shadow-md hover:bg-gradient-to-r from-gray-100 to-gray-300 hover:scale-110 transition-all duration-300"
         />
       </div>
     </header>
--- src/components/concert/ConcertHero.jsx
+++ src/components/concert/ConcertHero.jsx
@@ -1,7 +1,21 @@
+import Button from "../Button";
+
 export default function ConcertHero() {
   return (
-    <div>
-      <h1>hi</h1>
-    </div>
+    <section className="flex flex-col text-center items-center  text-white">
+      <div className="max-w-3xl mt-20 flex flex-col gap-4">
+        <h1 className="text-6xl font-sed">2000's Hip Hop and R&B</h1>
+        <p className="text-2xl tracking-wide">
+          Listen to the music you grew up with. Be with friends. Share memories
+          and create new ones. Lets party like a rockstar and lean back like its
+          our birthday!
+        </p>
+
+        <Button
+          btnTxt="Get Your Seat"
+          className="w-80 mx-auto bg-white text-black font-sed text-2xl px-5 py-2 rounded-full shadow-md hover:bg-gradient-to-r from-gray-100 to-gray-300 hover:scale-110 transition-all duration-300"
+        />
+      </div>
+    </section>
   );
 }
