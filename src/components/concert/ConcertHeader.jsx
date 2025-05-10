a176ea53f9b789c0863b1ee55c8a9ed0197a75fd
Davonne Vigil <davonnejv@gmail.com>
2025-05-09 22:36:45 -0500

    update header for Concert Project

--- src/components/concert/ConcertHeader.jsx
+++ src/components/concert/ConcertHeader.jsx
@@ -1,16 +1,18 @@
+import Button from "../Button";
+
 export default function ConcertHeader() {
   const ourHeader = [
     { link: "Tour Dates", id: 1 },
     { link: "Special Guest", id: 2 },
     { link: "Merch", id: 3 },
-    { link: "Get Tickets", id: 4 },
+    { link: "Community", id: 4 },
   ];
 
   return (
-    <header className=" text-white p-6">
+    <header className=" text-white p-6 shadow-md bg-black/10">
       <div className="flex justify-between items-center">
-        <h1 className="font-sed text-4xl md:text-6xl text-shadow-md transform hover:scale-105 transition-transform duration-300">
-          2000's Takin it Back Tour
+        <h1 className="font-sed text-3xl md:text-4xl text-shadow-md transform hover:scale-105 transition-transform duration-300">
+          🎧 Takin it Back Tour
         </h1>
         <ul className="hidden md:flex gap-10 font-sed text-2xl">
           {ourHeader.map((headerLink) => (
@@ -22,6 +24,10 @@ export default function ConcertHeader() {
             </li>
           ))}
         </ul>
+        <Button
+          btnTxt="Get Tickets"
+          className="bg-white text-black rounded-full p-2 font-sed text-2xl transition-all duration-300 transform hover:scale-110"
+        />
       </div>
     </header>
   );
--- src/components/concert/ConcertHero.jsx
+++ src/components/concert/ConcertHero.jsx
@@ -1,18 +1,7 @@
-import Image from "../Image";
 export default function ConcertHero() {
-  const time2025 = "./images/concert.jpg";
   return (
-    <section className="flex flex-col justify-center items-center">
-      <Image image={time2025} className="w-full max-w-full rounded-4xl p-2" />
-
-      <div className="flex flex-col gap-4 text-white mb-8 md:mb-0 md:top-60 md:left-48 font-cut absolute text-sm md:text-w-3xl md:text-5xl">
-        <p className=" bg-black -skew-4 p-4">Party of The Year</p>
-        <p className=" bg-black -skew-4 p-4">Tickets on Sale Now</p>
-      </div>
-      <Image
-        image="./images/time.png"
-        className="max-w-xs lg:max-w-xl absolute right-0 mt-60"
-      />
-    </section>
+    <div>
+      <h1>hi</h1>
+    </div>
   );
 }
