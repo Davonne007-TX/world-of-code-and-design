83934ee9ee8d31f384115eac25673b7016b9325e
Davonne Vigil <davonnejv@gmail.com>
2025-06-27 23:44:33 -0500

    feat: header with styles

---
+++ src/components/snowboarding/Contact.jsx
@@ -0,0 +1,14 @@
+export default function Contact() {
+  return (
+    <div className="flex gap-6 font-orb text-2xl">
+      <button className="relative group">
+        <span className="pb-1">Contact</span>
+        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-white transition-all duration-300 group-hover:w-full"></span>
+      </button>
+      <button className="relative group">
+        <span className="pb-1">Login</span>
+        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-white transition-all duration-300 group-hover:w-full"></span>
+      </button>
+    </div>
+  );
+}
--- src/components/snowboarding/ShredHeader.jsx
+++ src/components/snowboarding/ShredHeader.jsx
@@ -1,3 +1,4 @@
+import Contact from "./Contact";
 export default function ShredHeader() {
   const headerLinks = [
     {
@@ -20,23 +21,30 @@ export default function ShredHeader() {
       navItem: "Night Riding",
       id: 5,
     },
+    {
+      navItem: "Competitions",
+      id: 5,
+    },
   ];
 
   return (
-    <header className=" p-2 text-white flex justify-between items-center">
-      <h1 className="font-bold text-5xl">Shred Summit</h1>
-
-      <nav>
-        <ul>
+    <header className=" p-2 text-white flex flex-col gap-2 backdrop-blur-md bg-black/30 shadow-md">
+      <div className="flex justify-between items-center bg-black/40 p-2">
+        <h1 className="font-orb text-5xl">Shred Summit</h1>
+        <Contact />
+      </div>
+      <nav className="flex">
+        <ul className="hidden md:flex gap-10 text-black text-2xl font-rag p-2">
           {headerLinks.map((link) => (
-            <li key={link.id}>{link.navItem}</li>
+            <li
+              key={link.id}
+              className="cursor-pointer font-tit relative group"
+            >
+              <span className="pb-1">{link.navItem}</span>
+              <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-white transition-all duration-300 group-hover:w-full"></span>
+            </li>
           ))}
         </ul>
-
-        <div className="">
-          <button>Contact</button>
-          <button>Login</button>
-        </div>
       </nav>
     </header>
   );
