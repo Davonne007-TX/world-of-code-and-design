20e3306c1b93604133213d9ee38950a8b9d351e7
Davonne Vigil <davonnejv@gmail.com>
2025-06-28 09:38:27 -0500

    update hero section:

--- src/components/lets-ride/ShredHero.jsx
+++ src/components/lets-ride/ShredHero.jsx
@@ -5,7 +5,7 @@ export default function ShredHero() {
       <h2 className="text-4xl md:text-7xl font-orb bg-blue-200/10 shadow-sm max-w-3xl p-2">
         Go Big or Go Home
       </h2>
-      <div className="flex flex-col backdrop-blur-md gap-4 p-6 text-3xl md:text-3xl font-tit ml-4 bg-black/40 max-w-lg">
+      <div className="flex flex-col backdrop-blur-md gap-4 p-6 text-3xl md:text-3xl font-tit ml-4 bg-black/40 max-w-2xl">
         <p>
           A community that takes skateboarding to another level. Built on
           community, trust, and commitment.
