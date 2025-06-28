c8a8888e24e9073af605b610fdc6c2f72c344b86
Davonne Vigil <davonnejv@gmail.com>
2025-06-28 00:17:43 -0500

    update file names, add hero section with styles. update header with styles

--- src/components/bmx-club/GoBig.jsx
+++ src/components/bmx-club/GoBig.jsx
@@ -1,11 +1,11 @@
-import ShredHeader from "./ShredHeader.jsx";
+import GoBigHeader from "./GoBigHeader.jsx";
 import ShredHero from "./ShredHero.jsx";
 
 export default function GoBig() {
   return (
     <section className="bg-[url('./images/skateClub.jpg')] bg-cover min-h-screen">
       <div>
-        <ShredHeader />
+        <GoBigHeader />
         <ShredHero />
       </div>
     </section>
---
+++ src/components/bmx-club/ShredHero.jsx
@@ -0,0 +1,17 @@
+export default function ShredHero() {
+  return (
+    <div className="mt-60 text-white flex flex-col gap-4 ml-10">
+      {" "}
+      <h2 className="text-4xl md:text-7xl font-orb text-white bg-blue-200/4 p-2">
+        Go Big Home
+      </h2>
+      <p className="text-2xl md:text-3xl font-tit ml-4 bg-black/40 max-w-lg">
+        A community that take BMX riding to another level. Built on community,
+        trust, and commitment.
+      </p>
+      <p className="text-2xl md:text-3xl font-tit ml-4 bg-black/20 max-w-lg">
+        Ride. Compete. Make Memories.
+      </p>
+    </div>
+  );
+}
