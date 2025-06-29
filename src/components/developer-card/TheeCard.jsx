887a3b14eeb689a99eb97c8b7011a50fdd892f85
Davonne Vigil <davonnejv@gmail.com>
2025-06-28 22:16:40 -0500

    update: feat animated border

--- src/components/developer-card/TheeCard.jsx
+++ src/components/developer-card/TheeCard.jsx
@@ -7,12 +7,14 @@ export default function TheeCard() {
   const title = "Frontend Developer / UI Developer";
   return (
     <section className="max-w-xs md:max-w-lg md:ml-40 flex p-4 flex-col justify-center items-center">
-      <div className=" bg-white flex flex-col gap-4 rounded text-center border-4 border-neonGreen p-2 mt-20">
-        <Image image={me} className="p-2 rounded-2xl w-80 mx-auto" />
-        <h1 className="font-sig text-5xl">Davonne Vigil</h1>
-        <p className="text-2xl font-mono">{title}</p>
-        <TechStack />
-        <Offers />
+      <div className="rounded-lg animated-border-card">
+        <div className=" bg-white flex flex-col gap-4 rounded text-center p-2 mt-20 this-one">
+          <Image image={me} className="p-2 rounded-2xl w-80 mx-auto" />
+          <h1 className="font-sig text-5xl">Davonne Vigil</h1>
+          <p className="text-2xl font-mono">{title}</p>
+          <TechStack />
+          <Offers />
+        </div>
       </div>
     </section>
   );
