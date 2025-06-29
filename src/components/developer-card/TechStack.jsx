1ff264d54cf675e1626452b685692ab96da6e931
Davonne Vigil <davonnejv@gmail.com>
2025-06-28 19:24:35 -0500

    map over array of images instead

--- src/components/developer-card/TechStack.jsx
+++ src/components/developer-card/TechStack.jsx
@@ -1,35 +1,19 @@
 import Image from "../Image";
 
 export default function TechStack() {
-  const javaScript = "/images/js.png";
-  const reactImg = "/images/react.png";
-  const css = "/images/css.png";
-  const github = "/images/githubLogo.png";
-  const tailwind = "/images/tailwind.webp";
+  const techStackImages = [
+    { name: "JavaScript", src: "/images/js.png", id: 1 },
+    { name: "React", src: "/images/react.png", id: 2 },
+    { name: "CSS", src: "/images/css.png", id: 3 },
+    { name: "GitHub", src: "/images/githubLogo.png", id: 4 },
+    { name: "Tailwind", src: "/images/tailwind.webp", id: 5 },
+  ];
 
   return (
     <section className="flex justify-center items-center gap-2">
-      <Image
-        image={reactImg}
-        className="w-14"
-        alt="React Logo, image by icons8"
-      />
-      <Image
-        image={javaScript}
-        className="w-14"
-        alt="JavaScript Logo, image by icons8"
-      />
-      <Image image={css} className="w-14" alt="CSS Logo, image by icons8" />
-      <Image
-        image={github}
-        className="w-14"
-        alt="Github Logo, image by icons8"
-      />
-      <Image
-        image={tailwind}
-        className="w-14"
-        alt="Tailwind CSS logo, image by icons8"
-      />
+      {techStackImages.map((images) => (
+        <Image key={images.id} image={images.src} className="w-14" />
+      ))}
     </section>
   );
 }
