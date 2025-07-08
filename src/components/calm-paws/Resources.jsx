1a8852bc43fa6a4bea078a7900be367d11ac5aed
Davonne Vigil <davonnejv@gmail.com>
2025-07-07 23:07:20 -0500

    update: emergency contact info UI with styles

--- src/components/calm-paws/Resources.jsx
+++ src/components/calm-paws/Resources.jsx
@@ -14,18 +14,20 @@ export default function Resources() {
       name: "Your Vet",
       phone: "777-MY-VET",
       available: "Call for hours",
-      idL: 3,
+      id: 3,
     },
   ];
   return (
     <section className="flex flex-col justify-center items-center my-20">
       <div className="max-w-6xl">
-        <div className="flex gap-8">
+        <div className="flex gap-8 justify-center items-center">
           <FaHandHoldingMedical size={48} />
-          <h1 className="font-sig text-5xl">Resources</h1>
+          <h1 className="font-sig text-4xl lg:text-5xl">
+            Paw Emergency Resources
+          </h1>
         </div>
 
-        <div className="bg-red-200 border-2 border-red-600 mx-auto max-w-2xs md:max-w-4xl p-2 md:p-4 mt-10 rounded">
+        <div className="bg-red-200 border-2 border-red-600 mx-auto max-w-sm md:max-w-4xl lg::max-w-5xl p-2 md:p-8 mt-10 rounded">
           <div className="flex gap-8">
             <FaPhoneAlt size={28} className="mt-4 md:mt-0" />
             <h2 className="text-3xl font-serif font-bold text-red-600">
@@ -35,7 +37,14 @@ export default function Resources() {
 
           <ul className="mt-10 text-2xl flex flex-col gap-8">
             {emergencyContacts.map((contact) => (
-              <li key={contact.id}>{contact.name}</li>
+              <div key={contact.id} className="flex flex-col">
+                <div className="flex gap-10">
+                  <li className="text-3xl">{contact.name}</li>
+                  <button className="bg-red-400 p-1  ml-auto rounded text-md">
+                    {contact.phone}
+                  </button>
+                </div>
+              </div>
             ))}
           </ul>
         </div>
