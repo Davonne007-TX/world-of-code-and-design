a916b503142a5bab2a186ba9e176f70d258677b9
Davonne Vigil <davonnejv@gmail.com>
2025-07-08 22:13:44 -0500

    feat: React icons, dog and rocket with styles

--- src/components/calm-paws/GetInvolved.jsx
+++ src/components/calm-paws/GetInvolved.jsx
@@ -1,16 +1,25 @@
+import { FaDog } from "react-icons/fa";
+import { GiFireworkRocket } from "react-icons/gi";
+
 export default function GetInvolved() {
   const share = `Share awareness with family and friends about fireworks and the
           affects it has on our pets`;
 
-  const help = `  And how we can help them if they get distressed, during the holiday
-        celebration`;
+  const help = `And how we can help them if they get distressed, during the holiday`;
   return (
     <section className="flex flex-col gap-2 justify-center bg-blue-200 items-center min-h-screen">
       <div className="max-w-5xl flex flex-col gap-10 justify-center items-center">
         <h1 className="text-7xl font-sig">We need your help!</h1>
 
-        <p className="text-4xl text-center font-thin font-sans">{share}</p>
-        <p className="text-4xl text-center font-thin font-sans">{help}</p>
+        <div className="flex gap-2">
+          <FaDog size={48} />
+
+          <p className="text-4xl text-center font-thin font-sans">{share}</p>
+        </div>
+        <div className="flex gap-2">
+          <GiFireworkRocket size={48} />
+          <p className="text-4xl text-center font-thin font-sans">{help}</p>
+        </div>
       </div>
     </section>
   );
