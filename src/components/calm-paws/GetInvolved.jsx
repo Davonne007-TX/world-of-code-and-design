bf2512b139acf44242a4d42646c8028f3031ac9d
Davonne Vigil <davonnejv@gmail.com>
2025-07-08 22:26:35 -0500

    feat: motion.dev animations for call to action GetInvolved component, update: mobile responsiveness

--- src/components/calm-paws/GetInvolved.jsx
+++ src/components/calm-paws/GetInvolved.jsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import { FaDog } from "react-icons/fa";
 import { GiFireworkRocket } from "react-icons/gi";
 
@@ -8,19 +9,29 @@ export default function GetInvolved() {
   const help = `And how we can help them if they get distressed, during the holiday`;
   return (
     <section className="flex flex-col gap-2 justify-center bg-blue-200 items-center min-h-screen">
-      <div className="max-w-5xl flex flex-col gap-10 justify-center items-center">
-        <h1 className="text-7xl font-sig">We need your help!</h1>
+      <motion.button
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 1 }}
+        className="max-w-5xl flex flex-col gap-10 justify-center items-center"
+      >
+        <h1 className="text-3xl md:text-7xl font-sig">We need your help!</h1>
 
         <div className="flex gap-2">
           <FaDog size={48} />
 
-          <p className="text-4xl text-center font-thin font-sans">{share}</p>
+          <p className="text-2xl md:text-4xl text-center font-thin font-sans">
+            {share}
+          </p>
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-1">
           <GiFireworkRocket size={48} />
-          <p className="text-4xl text-center font-thin font-sans">{help}</p>
+          <p className="text-2xl md:text-4xl text-center font-thin font-sans">
+            {help}
+          </p>
         </div>
-      </div>
+      </motion.button>
     </section>
   );
 }
