0edec6ffc7421bb202c9180007704d5bb35fe481
Davonne Vigil <davonnejv@gmail.com>
2025-07-10 23:08:22 -0500

    update: styles on helpful links, hovers, padding, color,  all that good stuff

--- src/components/calm-paws/HelpfulLinks.jsx
+++ src/components/calm-paws/HelpfulLinks.jsx
@@ -21,19 +21,24 @@ export default function HelpfulLinks() {
       id: 4,
     },
     {
-      text: "Dog Topia Blog to help dogs get through nights of fireworks",
-      link: "https://www.dogtopia.com/blog/5-tips-help-dog-get-nights-fireworks/",
+      text: "petMd by Chewy Helpful Tips",
+      link: "https://www.petmd.com/dog/tips-helping-dogs-thats-scared-fireworks",
       id: 5,
+    },
+    {
+      text: "College of Veterinary Medicine",
+      link: "https://www.vet.cornell.edu/departments-centers-and-institutes/riney-canine-health-center/canine-health-information/fear-fireworks-and-thunderstorms",
+      id: 6,
     },
   ];
   return (
     <section className="flex flex-col justify-center py-20 items-center">
       <div className="max-w-xs md:max-w-sm lg:max-w-6xl flex flex-col gap-8 ">
-        <ul className="w-full max-w-7xl px-6 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <ul className="w-full max-w-7xl px-6 grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {resources.map((resource) => (
             <li
               key={resource.id}
-              className="text-2xl md:text-3xl flex items-center p-2 font-thin cursor-pointer shadow-md hover:shadow-xl transition duration-300 hover:scale-[1.02] border border-gray-200 hover:text-blue-400"
+              className="text-2xl md:text-3xl flex items-center p-4 font-thin cursor-pointer py-10 shadow-md hover:shadow-xl transition duration-300 hover:scale-[1.02] border border-gray-200 hover:text-blue-400"
             >
               {" "}
               <a href={resource.link}>{resource.text}</a>
