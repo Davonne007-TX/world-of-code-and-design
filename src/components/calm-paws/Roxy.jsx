29370329ce8549e68a2daa4997b6d641541f6c37
Davonne Vigil <davonnejv@gmail.com>
2025-07-19 02:27:17 -0500

    add cloud thought to Roxy's image

--- src/components/calm-paws/Roxy.jsx
+++ src/components/calm-paws/Roxy.jsx
@@ -3,10 +3,19 @@ import Image from "../Image";
 export default function Roxy() {
   return (
     <section className="my-20 flex justify-center items-center">
-      <Image
-        image="./images/roxy2.png"
-        className="max-w-xs md:max-w-3xl lg:max-w-5xl relative rounded-3xl"
-      />
+      <div className="relative">
+        <Image
+          image="./images/cloud.png"
+          className="absolute top-40 max-w-sm lg:max-w-md text-center text-sm font-semibold text-black "
+        />
+        <blockquote className="relative top-76 lg:top-80 left-30 font-sig flex-wrap text-2xl lg:text-4xl w-40">
+          <cite> "Not these fireworks again!"</cite>
+        </blockquote>
+        <Image
+          image="./images/roxy2.png"
+          className="max-w-xs md:max-w-3xl lg:max-w-5xl rounded-3xl "
+        />
+      </div>
     </section>
   );
 }
