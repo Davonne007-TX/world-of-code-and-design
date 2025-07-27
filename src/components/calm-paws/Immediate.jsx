28a5d06d4db880ab327ba09dd9fc1ededf257aa3
Davonne Vigil <davonnejv@gmail.com>
2025-07-26 23:55:06 -0500

    add: UI for Immediate Help Tips with MUI

--- src/components/calm-paws/Immediate.jsx
+++ src/components/calm-paws/Immediate.jsx
@@ -1,10 +1,33 @@
-import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import TabContext from "@mui/lab/TabContext";
-import { useState } from "react";
+import ImmediateCard from "./ImmediateCard";
+
+const quickActions = [
+  {
+    icon: "🎶",
+    title: "Smooth Music",
+    description: "Play calming smooth music",
+  },
+  {
+    icon: "🎾",
+    title: "Distraction",
+    description: "Chew toys, toys they love",
+  },
+  {
+    icon: "🏠",
+    title: "Stay indoors",
+    description: "Make sure dogs stay inside",
+  },
+  {
+    icon: "💤",
+    title: "Tire Them Out",
+    description: "Exercise before fireworks start",
+  },
+];
 
 export default function Immediate() {
   const [value, setValue] = useState("1");
@@ -35,7 +58,27 @@ export default function Immediate() {
           </TabList>
         </Box>
 
-        <TabPanel value="1">Immediate Help Content</TabPanel>
+        <TabPanel value="1">
+          <div className="max-w-2xl p-4 rounded-md mt-10 mx-auto bg-blue-200">
+            <ImmediateCard
+              immediateTitle="Quick Actions"
+              description="Take these steps now to help"
+              color="text-slate-800"
+              descriptionColor="text-blue-600"
+            />
+            <ul className="p-2 flex flex-col gap-4">
+              {quickActions.map((action, index) => (
+                <li key={index} className="bg-white p-4 rounded">
+                  <div className="flex gap-2">
+                    <span className="text-2xl">{action.icon}</span>
+                    <h2 className="font-bold text-xl">{action.title}</h2>
+                  </div>
+                  <p className="text-lg ml-8">{action.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </TabPanel>
         <TabPanel value="2">Calming Techniques Content</TabPanel>
       </TabContext>
     </section>
