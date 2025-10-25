3ac150b670557f0acb0f79f4d02134beef4eb6a0
Davonne Vigil <davonnejv@gmail.com>
2025-10-24 23:59:19 -0500

    feature: header and background image

--- src/components/robot/SuperRobot.jsx
+++ src/components/robot/SuperRobot.jsx
@@ -1,7 +1,22 @@
 export default function SuperRobot() {
+  const navItems = [
+    { link: "Mission", id: 0, path: "" },
+    { link: "Features", id: 1, path: "" },
+    { link: "Ethics", id: 2, path: "" },
+    { link: "Support ", id: 3, path: "" },
+    { link: "Shop", id: 4, path: "" },
+  ];
   return (
-    <section>
-      <h1>Super Robot</h1>
-    </section>
+    <header className="bg-[url('./images/superRobot.jpg')] bg-cover bg-center min-h-screen">
+      <h1 className="text-4xl">Super Robot</h1>
+
+      <nav>
+        <ul>
+          {navItems.map((item) => (
+            <li key={item.id}>{item.link}</li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
