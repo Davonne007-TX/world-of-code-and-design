5d2131498b853a110543caf43e532e83b1d519d4
Davonne Vigil <davonnejv@gmail.com>
2025-10-25 17:58:23 -0500

    feature: sign in and get started button, add hover effect

--- src/components/robot/SuperRobot.jsx
+++ src/components/robot/SuperRobot.jsx
@@ -3,26 +3,33 @@ export default function SuperRobot() {
     { link: "Mission", id: 0, path: "" },
     { link: "Features", id: 1, path: "" },
     { link: "Ethics", id: 2, path: "" },
-    { link: "Support ", id: 3, path: "" },
+    { link: "Gallery ", id: 3, path: "" },
     { link: "Shop", id: 4, path: "" },
   ];
   return (
     <header className="bg-[url('./images/superRobot.jpg')] bg-cover bg-center min-h-screen text-white">
-      <div className="flex gap-4 items-center p-1 bg-gradient-to-b from-purple-300/80 to-purple-600">
-        <img
-          src="./images/robot.png"
-          className="text-4xl w-20 drop-shadow-[0_0_10px_#f755c1"
-        />
-        <h1 className="text-xl font-press shadow-lg p-2 shadow-pink-200">
-          SuperRobo
-        </h1>
-        <nav>
-          <ul className="flex gap-8 p-2 text-gray-200 text-lg font-serif">
+      <div className="flex items-center justify-between p-2 bg-gradient-to-b from-black to-pink-500/20">
+        <div className="flex items-center gap-4">
+          <img
+            src="./images/robot.png"
+            className="text-4xl w-20 drop-shadow-[0_0_10px_#f755c1"
+          />
+          <h1 className="text-xl font-press shadow-pink-200">SuperRobo</h1>
+        </div>
+        <nav className="flex gap-8 text-gray-200 text-lg font-serif ml-8">
+          <ul className="hidden lg:flex gap-8">
             {navItems.map((item) => (
               <li key={item.id}>{item.link}</li>
             ))}
           </ul>
         </nav>
+
+        <div className="flex gap-4 ml-auto text-lg">
+          <button className="px-4 py-2 cursor-pointer ">Sign In</button>
+          <button className="px-4 py-2 border-1 border-pink-400 cursor-pointer rounded hover:shadow-[0_0_15px_#ec4899] hover:bg-pink-600 transition-all duration-300">
+            Get started
+          </button>
+        </div>
       </div>
     </header>
   );
