51b4d6a054c19d66d1b71d688d8e1693c9c60d64
Davonne Vigil <davonnejv@gmail.com>
2025-10-25 18:58:34 -0500

    add: call to action button with style

--- src/components/robot/SuperHero.jsx
+++ src/components/robot/SuperHero.jsx
@@ -2,16 +2,23 @@ export default function SuperHero() {
   return (
     <section className="flex flex-col gap-8 justify-center items-center mt-20 px-6">
       <div className="max-w-xl lg:max-w-2xl text-center">
+        <button className="bg-neutral-200 font-semibold text-pink-400 rounded-full text-lg tracking-widest px-6 mb-4 p-2">
+          AI Powered Robot
+        </button>
         <h2 className=" text-4xl md:text-5xl font-orb font-semibold leading-snug text-white drop-shadow-[0_0_10px_#ec4899]">
           The Future of Home Living{" "}
-          <span className="bg-pink-500 px-3 py-2 skew-x-8 inline-block text-white">
+          <span className="bg-pink-400 px-3 py-2 skew-x-8 inline-block text-white">
             is here
           </span>
         </h2>
         <p className="mt-6 text-lg md:text-2xl text-gray-200/90 max-w-xl mx-auto">
-          Meet <span className="text-purple-400">SuperRobo,</span> your own
-          custom Robot, designed to simplify daily life, one task at a time.
+          Meet <span className="font-bold text-pink-500">SuperRobo,</span> your
+          own custom Robot, designed to simplify daily life, one task at a time.
         </p>
+
+        <button className="mt-8 bg-black/80 font-orb text-xl cursor-pointer w-80 p-2 rounded">
+          Learn More →
+        </button>
       </div>
     </section>
   );
--- src/components/robot/SuperRobot.jsx
+++ src/components/robot/SuperRobot.jsx
@@ -3,7 +3,7 @@ import SuperHero from "./SuperHero";
 
 export default function SuperRobot() {
   return (
-    <main className="bg-[url('./images/superRobot.jpg')] bg-cover bg-center min-h-screen text-white">
+    <main className="bg-[url('./images/superRobot.jpg')] bg-cover bg-center sm:bg-top md:bg-center min-h-screen text-white">
       <SuperHeader />
       <SuperHero />
     </main>
