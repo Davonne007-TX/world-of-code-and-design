e5ccccbc4055ac55501537015a5a8df524bcfb1f
Davonne Vigil <davonnejv@gmail.com>
2025-11-14 23:44:18 -0600

    map over array of packages, display offers with dummy text

--- src/components/waxing/Packages.jsx
+++ src/components/waxing/Packages.jsx
@@ -1,3 +1,5 @@
+import { packages } from "./data/packages";
+
 export default function Packages() {
   return (
     <section className="flex flex-col justify-center items-center py-20">
@@ -14,6 +16,20 @@ export default function Packages() {
             We offer a variety of packages to keep track of your waxing progress
             and help you save money along the way!
           </p>
+
+          <ul>
+            {packages.map((pkg) => (
+              <>
+                <div className="flex flex-col gap-8">
+                  <img src={pkg.src} alt={pkg.alt} />
+                  <div className="flex flex-col gap-2">
+                    <h2 key={pkg.id}>{pkg.name}</h2>
+                    <p>{pkg.description}</p>
+                  </div>
+                </div>
+              </>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
---
+++ src/components/waxing/data/packages.js
@@ -0,0 +1,26 @@
+export const packages = [
+  {
+    name: "College Package",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis odio sit amet enim elementum pulvinar. Donec at rutrum leo, vitae feugiat velit.",
+    src: "",
+    alt: "",
+    id: 0,
+  },
+  {
+    name: "Dream Package",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis odio sit amet enim elementum pulvinar. Donec at rutrum leo, vitae feugiat velit.",
+    src: "",
+    alt: "",
+    id: 1,
+  },
+  {
+    name: "Royalty Package",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis odio sit amet enim elementum pulvinar. Donec at rutrum leo, vitae feugiat velit.",
+    src: "",
+    alt: "",
+    id: 2,
+  },
+];
