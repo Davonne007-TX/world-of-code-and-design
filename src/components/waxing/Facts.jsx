b15c2bde665d42158e5e1b712aa1ff3eb967bd66
Davonne Vigil <davonnejv@gmail.com>
2025-11-16 17:55:58 -0600

    map: lets make it cleaner. Map over questions for MUI accordin component

--- src/components/waxing/Facts.jsx
+++ src/components/waxing/Facts.jsx
@@ -3,6 +3,7 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { faqs } from "./data/faq";
 
 export default function Facts() {
   return (
@@ -10,114 +11,21 @@ export default function Facts() {
       <div className="flex flex-col justify-center items-center">
         <h2 className="font-glo text-4xl">Frequently Asked Questions</h2>
         <div className="mt-10">
-          <Accordion>
-            <AccordionSummary
-              expandIcon={
-                <ArrowDropDownIcon className="text-candy rounded-3xl" />
-              }
-              aria-controls="panel2-content"
-              id="panel2-header"
-            >
-              <Typography component="span">
-                How old do I need to be to get a wax?
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
+          {faqs.map((question, id) => (
+            <Accordion key={id}>
+              <AccordionSummary
+                expandIcon={<ArrowDropDownIcon className="text-candy" />}
+              >
+                <Typography>{question.question}</Typography>
+              </AccordionSummary>
 
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ArrowDropDownIcon className="text-candy" />}
-              aria-controls="panel2-content"
-              id="panel2-header"
-            >
-              <Typography component="span">
-                What kind of wax do you use?
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ArrowDropDownIcon />}
-              aria-controls="panel2-content"
-              id="panel2-header"
-            >
-              <Typography component="span">Accordion 2</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ArrowDropDownIcon />}
-              aria-controls="panel2-content"
-              id="panel2-header"
-            >
-              <Typography component="span">Accordion 2</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ArrowDropDownIcon />}
-              aria-controls="panel2-content"
-              id="panel2-header"
-            >
-              <Typography component="span">Accordion 2</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ArrowDropDownIcon />}
-              aria-controls="panel2-content"
-              id="panel2-header"
-            >
-              <Typography component="span">Accordion 2</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
+              <AccordionDetails>
+                <Typography>{question.answer}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))}
         </div>
       </div>
     </section>
   );
 }
-
-//I think we can just map over this no???
-//I have like so many
