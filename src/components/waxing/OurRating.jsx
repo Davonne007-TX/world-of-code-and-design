7c97bbec2b7c96aaf5a9a44d6fd577d858477d3e
Davonne Vigil <davonnejv@gmail.com>
2025-11-16 23:41:55 -0600

    add images to review cards

--- src/components/waxing/OurRating.jsx
+++ src/components/waxing/OurRating.jsx
@@ -7,12 +7,14 @@ export default function OurRating() {
   const [value, setValue] = useState(5);
 
   return (
-    <section className="max-w-6xl p-8 mx-auto my-20">
-      <div className="flex flex-col justify-center items-center gap-4 text-center">
+    <section className="max-w-6xl p-8 mx-auto mt-20">
+      <div className="flex flex-col justify-center items-center gap-4 text-center my-20">
         <h2 className="mt-8 text-3xl md:text-4xl font-bold font-glo">
-          What Our Guests Say
+          What Our Guests Have to Say
         </h2>
-        <p className="text-lg md:text-2xl">We treat every guests like family</p>
+        <p className="text-lg md:text-2xl">
+          We aim high to meet your beauty needs
+        </p>
 
         <ul className="flex flex-col lg:flex-row justify-center items-center mt-10 gap-12 font-thin text-xl">
           {reviews.map((review, i) => (
@@ -28,13 +30,19 @@ export default function OurRating() {
               viewport={{ once: true }}
               className="bg-white shadow-md cursor-pointer hover:scale-105 rounded-2xl p-6 max-w-xs flex flex-col items-center text-center gap-4"
             >
+              <img
+                src={review.src}
+                alt={review.id}
+                className="w-24 h-24 rounded-full shadow-md"
+              />
+
+              <p>{review.review}</p>
               <Rating
                 name={`rating-${review.id}`}
                 defaultValue={value}
                 readOnly
               />
-              <p>{review.review}</p>
-              <p className="font-bold">- {review.name}</p>
+              <p className="font-bold">{review.name}</p>
             </motion.li>
           ))}
         </ul>
--- src/components/waxing/data/reviews.js
+++ src/components/waxing/data/reviews.js
@@ -11,14 +11,15 @@ export const reviews = [
     review:
       "I'll definitely will be back! Rosalie was so nice and really knew what she was doing!   ",
     name: "Alice Cullen",
-    src: "Girl with make up, short hair, and a hat, image by mahdi chaghari on Unsplash",
+    src: "./images/alice.jpg",
+    alt: "Girl with make up, short hair, and a hat, image by mahdi chaghari on Unsplash",
     id: 1,
   },
   {
     review:
       "I was in town for a wedding, and Renesmee was able to fit me in! Super fast and painless!",
     name: "Jessica Stanley",
-    src: "./images/jessica.jpg",
+    src: "./images/jessica9.jpg",
     alt: "Girl smiling with long bronze hair in the garden, image by Ayo Ogunseinde",
 
     id: 2,
