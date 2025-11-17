8d83ee452c7d8b729fbc102ec7638e040f94b2d6
Davonne Vigil <davonnejv@gmail.com>
2025-11-16 23:05:16 -0600

    add images for reviews and add images to javascript array with alt text

--- src/components/waxing/data/reviews.js
+++ src/components/waxing/data/reviews.js
@@ -1,20 +1,26 @@
 export const reviews = [
   {
     review:
-      "Amazing! Angela took care of me and made sure my eyebrows were just the way I want!",
+      "Amazing! Angela took care of me and made sure my eyebrows were stunning",
     name: "Bella Swan",
+    src: "./images/bella.jpg",
+    alt: "Girl with make up and dark hair with tattoos, image from sobhan joodi on Unsplash",
     id: 0,
   },
   {
     review:
       "I'll definitely will be back! Rosalie was so nice and really knew what she was doing!   ",
     name: "Alice Cullen",
+    src: "Girl with make up, short hair, and a hat, image by mahdi chaghari on Unsplash",
     id: 1,
   },
   {
     review:
       "I was in town for a wedding, and Renesmee was able to fit me in! Super fast and painless!",
     name: "Jessica Stanley",
+    src: "./images/jessica.jpg",
+    alt: "Girl smiling with long bronze hair in the garden, image by Ayo Ogunseinde",
+
     id: 2,
   },
 ];
