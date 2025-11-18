b8c4aa1b150b7c970c558dab5fe2a39d20f15bdf
Davonne Vigil <davonnejv@gmail.com>
2025-11-18 11:30:26 -0600

    footer: header, text, book appointment

--- src/components/waxing/SmoothFooter.jsx
+++ src/components/waxing/SmoothFooter.jsx
@@ -1,7 +1,14 @@
 export default function SmoothFooter() {
   return (
-    <footer className="bg-black">
-      <h2 className="text-candy font-glo">Silky Smooth</h2>
+    <footer className="bg-black flex flex-col justify-center items-center gap-4 p-8">
+      <h2 className="text-candy font-cut text-5xl">Silky Smooth</h2>
+      <p className="text-white text-xl">
+        Premium Waxing Services just for you. Book Your Appointment and get
+        Silky Smooth Today.
+      </p>
+      <button className="bg-candy font-bold p-2 rounded-full">
+        Book Your Appointment
+      </button>
     </footer>
   );
 }
