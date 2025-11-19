2362f5422e7004a7b48d015e3ee7aeb9419ccaea
Davonne Vigil <davonnejv@gmail.com>
2025-11-18 23:23:26 -0600

    add: social media icons, map over and display icons

--- src/components/waxing/SmoothFooter.jsx
+++ src/components/waxing/SmoothFooter.jsx
@@ -1,14 +1,31 @@
 export default function SmoothFooter() {
+  const footerSocialMediaLinks = [
+    { src: "./images/insta.png", alt: "Instagram icon, by icons8", id: 0 },
+    {
+      src: "./images/twitter.png",
+      alt: "Twitter bird logo, icon by icons 8",
+      id: 1,
+    },
+
+    {
+      src: "./images/fb.png",
+      alt: "Facebook logo, icon by icons 8",
+      id: 2,
+    },
+  ];
   return (
     <footer className="bg-black flex flex-col justify-center items-center gap-4 p-8">
       <h2 className="text-candy font-cut text-5xl">Silky Smooth</h2>
-      <p className="text-white text-xl">
+      <p className="text-white text-xl text-center max-w-xl">
         Premium Waxing Services just for you. Book Your Appointment and get
         Silky Smooth Today.
       </p>
-      <button className="bg-candy font-bold p-2 rounded-full">
-        Book Your Appointment
-      </button>
+
+      <ul className="flex gap-4">
+        {footerSocialMediaLinks.map((mediaLinks) => (
+          <img key={mediaLinks.id} src={mediaLinks.src} alt={mediaLinks.alt} />
+        ))}
+      </ul>
     </footer>
   );
 }
