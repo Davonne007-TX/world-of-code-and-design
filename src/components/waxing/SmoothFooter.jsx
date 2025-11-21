d86bec843d7a75e43bfee371ec56252029a073ed
Davonne Vigil <davonnejv@gmail.com>
2025-11-21 00:06:00 -0600

    map over footer array instead, make everything look nicer

--- src/components/waxing/SmoothFooter.jsx
+++ src/components/waxing/SmoothFooter.jsx
@@ -1,24 +1,12 @@
-export default function SmoothFooter() {
-  const footerSocialMediaLinks = [
-    { src: "./images/insta.png", alt: "Instagram icon, by icons8", id: 0 },
-    {
-      src: "./images/twitter.png",
-      alt: "Twitter bird logo, icon by icons 8",
-      id: 1,
-    },
+import { footerSocialMediaLinks, quickLinks } from "./data/footer";
 
-    {
-      src: "./images/fb.png",
-      alt: "Facebook logo, icon by icons 8",
-      id: 2,
-    },
-  ];
+export default function SmoothFooter() {
   return (
     <footer className="bg-black flex flex-col justify-center items-center gap-4 p-8">
       <h2 className="text-candy font-cut text-5xl font-bold">Silky Smooth</h2>
       <p className="text-white text-2xl text-center max-w-xl">
-        Premium Waxing Services from head to toe. Book Your Appointment and get
-        Silky Smooth Today with our waxing experts.
+        Premium Waxing Services from head to toe. Book your appointment and get
+        silky smooth today with our waxing experts.
       </p>
 
       <number className="text-white text-xl">123-456-7890</number>
@@ -29,12 +17,15 @@ export default function SmoothFooter() {
         ))}
       </ul>
 
-      <ul className="text-white flex gap-4 text-lg font-thin">
-        <li className=" hover:text-candy">About | </li>
-        <li className=" hover:text-candy">Services | </li>
-        <li className=" hover:text-candy">Packages | </li>
-        <li className=" hover:text-candy">FAQS | </li>
-        <li className=" hover:text-candy">Contact </li>
+      <ul className="text-white flex items-center gap-4 text-lg font-light">
+        {quickLinks.map((link) => (
+          <li
+            key={link.id}
+            className="cursor-pointer  hover:font-bold transition-colors after:content-['|'] after:ml-4 last:after:content-none"
+          >
+            {link.name}
+          </li>
+        ))}
       </ul>
     </footer>
   );
---
+++ src/components/waxing/data/footer.js
@@ -0,0 +1,22 @@
+export const footerSocialMediaLinks = [
+  { src: "./images/insta.png", alt: "Instagram icon, by icons8", id: 0 },
+  {
+    src: "./images/twitter.png",
+    alt: "Twitter bird logo, icon by icons 8",
+    id: 1,
+  },
+
+  {
+    src: "./images/fb.png",
+    alt: "Facebook logo, icon by icons 8",
+    id: 2,
+  },
+];
+
+export const quickLinks = [
+  { name: "About", id: 0 },
+  { name: "Services", id: 1 },
+  { name: "Packages", id: 2 },
+  { name: "Faqs", id: 3 },
+  { name: "Contact", id: 4 },
+];
