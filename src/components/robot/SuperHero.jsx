632bf86cee37d50a29be4f2a3e6f78275960d3f6
Davonne Vigil <davonnejv@gmail.com>
2025-11-22 20:53:44 -0600

    align: hero section

--- src/components/robot/SuperHero.jsx
+++ src/components/robot/SuperHero.jsx
@@ -1,8 +1,8 @@
 export default function SuperHero() {
   return (
-    <section className="flex flex-col gap-8 justify-center items-center mt-20 px-6">
+    <section className="flex flex-col gap-8 justify-center items-center mt-12 px-6">
       <div className="max-w-xl lg:max-w-2xl text-center">
-        <button className="bg-neutral-200 font-semibold text-pink-400 rounded-full text-lg tracking-widest px-6 mb-4 p-2">
+        <button className="bg-neutral-200 font-semibold text-pink-500 rounded-full text-lg tracking-widest px-6 mb-4 p-2">
           AI Powered Robot
         </button>
         <h2 className=" text-4xl md:text-5xl font-orb font-semibold leading-snug text-white drop-shadow-[0_0_10px_#ec4899]">
@@ -11,12 +11,12 @@ export default function SuperHero() {
             is here
           </span>
         </h2>
-        <p className="mt-4 text-lg md:text-2xl text-gray-200/90 max-w-xl mx-auto">
+        <p className="mt-8 text-lg md:text-2xl text-gray-200/90 max-w-xl mx-auto">
           Meet <span className="font-bold text-pink-500">SuperRobo,</span> your
           own custom Robot, designed to simplify daily life, one task at a time.
         </p>
 
-        <button className="mt-8  bg-gradient-to-b from-black via-[#1a1a1a] to-black font-orb text-xl cursor-pointer w-80 p-2 rounded">
+        <button className=" mt-4 bg-gradient-to-b from-black via-[#1a1a1a] to-black font-orb text-xl cursor-pointer w-80 p-2 rounded">
           Learn More →
         </button>
       </div>
