161c5e0f08621e7c39be55e758d1ba9b0c928bf5
Davonne Vigil <davonnejv@gmail.com>
2025-12-23 23:59:05 -0600

    add: take action buttons, feature mobile responsiveness with style

--- src/components/our-planet/OurPlanet.jsx
+++ src/components/our-planet/OurPlanet.jsx
@@ -5,7 +5,7 @@ export default function OurPlanet() {
     <main>
       <PlanetNav />
       <div className="relative bg-[url('/images/earth.webp')] bg-cover bg-center min-h-screen">
-        <div className="absolute inset-0 bg-gradient-to-b from-black/70 via-black/40 to-black/90"></div>
+        <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/60 to-black/80"></div>
 
         <div className="relative z-50 flex flex-col justify-center items-center min-h-screen">
           <div className="text-center space-y-6">
@@ -13,16 +13,27 @@ export default function OurPlanet() {
               CLIMATE EMERGENCY
             </button>
 
-            <h2 className=" text-white text-6xl font-bold md:text-8xl">
+            <h2 className=" text-white text-center text-4xl font-bold md:text-8xl">
               Our Planet.
               <br />
-              <span className="text-blue-500">Our Responsibility.</span>
+              <span className="text-blue-500 md:ml-20 text-center">
+                Our Responsibility.
+              </span>
             </h2>
-            <p className="text-white mx-auto text-center max-w-2xl text-2xl bg-black/10 shadow-md p-2">
+            <p className="text-gray-200 mx-auto text-center max-w-2xl md:text-2xl bg-black/10 shadow-md p-2">
               At the moment, we only have one planet. One home. One Earth. It is
               up to us to take care of it and put a stop to our climate crisis.
               The time for change is now.{" "}
             </p>
+
+            <div className="flex flex-col md:flex-row justify-center items-center gap-8">
+              <button className="bg-blue-400 p-2 text-white">
+                Ready to take action
+              </button>
+              <button className="bg-black/40 p-4 text-white">
+                Check out the data
+              </button>
+            </div>
           </div>
         </div>
       </div>
